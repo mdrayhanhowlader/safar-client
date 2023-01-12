9b6907bfd50b9d604928ec300e5ade2eb1b6b06e
syed abdul hakim <s.abdulhakim10@gmail.com>
2023-01-12 16:48:58 +0600

    page crated

---
+++ src/pages/Home/Offers/Offers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Offers = () => {
+    return (
+        <div>
+            <h2 className="text-2xl font-semibold">Offers</h2>
+        </div>
+    );
+};
+
+export default Offers;
