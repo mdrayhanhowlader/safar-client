ea73b48f59ed541d832dad57cccb66f94a09770f
webnur <mdnuruli291@gmai.com>
2023-01-16 11:19:36 +0600

    navber background color fixed

--- src/pages/Shared/Navbar/Navbar.js
+++ src/pages/Shared/Navbar/Navbar.js
@@ -6,26 +6,23 @@ import { FaUser } from "react-icons/fa";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <nav
-      x-data="{ isOpen: false }"
-      className="relative bg-gray-800"
-    >
+    <nav x-data="{ isOpen: false }" className="relative bg-slate-50">
       <div className="container px-6 py-4 mx-auto">
         <div className="lg:flex lg:items-center lg:justify-between">
           <div className="flex items-center justify-between">
             <div className="text-xl font-semibold text-gray-700">
               <a
-                className="text-2xl font-bold transition-colors duration-300 transform lg:text-3xl text-white hover:text-gray-700"
+                className="text-2xl font-bold transition-colors duration-300 transform lg:text-3xl hover:text-gray-700"
                 href="/"
               >
-                Safar
+                SAFAR
               </a>
             </div>
 
             <div onClick={() => setOpen(!open)} className="flex lg:hidden">
               <button
                 type="button"
-                className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+                className="text-gray-500 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
                 aria-label="toggle menu"
               >
                 {open ? (
@@ -38,7 +35,7 @@ const Navbar = () => {
           </div>
 
           <div
-            className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-base-200  dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center ${
+            className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-gray-50 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center ${
               open
                 ? "translate-x-0 opacity-100 "
                 : "opacity-0 -translate-x-full"
@@ -47,28 +44,28 @@ const Navbar = () => {
             <div className="flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-8">
               <a
                 href="/"
-                className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
+                className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 hover:bg-gray-100"
               >
                 Home
               </a>
 
               <a
                 href="/blogs"
-                className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
+                className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 hover:bg-gray-100"
               >
                 Blogs
               </a>
 
               <a
                 href="/login"
-                className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
+                className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 hover:bg-gray-100"
               >
                 Login
               </a>
               <a
                 href="/"
                 to="/signup"
-                className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
+                className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 hover:bg-gray-100"
               >
                 SignUp
               </a>
