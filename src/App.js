0ab3ca848396da1659541e277d27e59446e417ba
mdrayhanhowlader <rhowlader5314@gmail.com>
2023-01-17 19:28:20 +0600

    rayhan branch updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="">
-      <article className="prose">
-        <h1>Test Typography</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-          obcaecati, laborum explicabo doloremque repudiandae dicta mollitia
-          impedit sint ratione placeat nemo quia ipsum eaque praesentium hello
-          deullo kjaofao voluptates perferendis fugiat adipisci delectus!
-        </p>
-      </article>
-    </div>
-  );
+  return <div className=""></div>;
 }
 
 export default App;
