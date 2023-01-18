62060b28a0469b50b53af0f1dbc2762276beb763
mdrayhanhowlader <rhowlader5314@gmail.com>
2023-01-18 14:38:25 +0600

    updated all folder structure of search page

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SearchPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>hello search</h2>
+    </div>
+  );
 };
 
 export default SearchPage;
