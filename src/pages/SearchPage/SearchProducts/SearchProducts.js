f323a7da9c1e84e651a00607c9c139a63daae801
mdrayhanhowlader <rhowlader5314@gmail.com>
2023-01-19 05:30:58 +0600

    search navbar added

--- src/pages/SearchPage/SearchProducts/SearchProducts.js
+++ src/pages/SearchPage/SearchProducts/SearchProducts.js
@@ -1,9 +1,12 @@
 import React from "react";
+import SearchNav from "./SearchNav";
 
 const SearchProducts = () => {
   return (
-    <div className="w-full my-6">
-      <h2>search products side</h2>
+    <div className="w-full mt-12">
+      <div className="md:float-right pr-6">
+        <SearchNav></SearchNav>
+      </div>
     </div>
   );
 };
