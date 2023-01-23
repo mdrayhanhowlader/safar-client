b15c5c77c2a4d3171763eb0f788ec8d52d5f85b0
himel-shimul <himelshimul00@gmail.com>
2023-01-23 23:58:32 +0600

    offerpage add

---
+++ src/pages/OfferPage/OfferPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import HeaderSearch from '../Home/HeaderSearch/HeaderSearch';
+import Subscribe from '../Home/Subscibe/Subscribe';
+import CityCard from './CityCard/CityCard';
+
+const OfferPage = () => {
+    return (
+        <div className='w-[80%] mx-auto'>
+            <HeaderSearch></HeaderSearch>
+            <h2 className='text-xl my-2 mt-20'>Explore more Early 2023 Deals</h2>
+            <div className="grid lg:grid-cols-3 gap-6">
+            <CityCard></CityCard>
+            <CityCard></CityCard>
+            <CityCard></CityCard>
+            <CityCard></CityCard>
+            <CityCard></CityCard>
+            <CityCard></CityCard>
+            </div>
+            <Subscribe></Subscribe>
+        </div>
+    );
+};
+
+export default OfferPage;
