2c1cf900a153eaa007d8ce79a96bd3e7676137d4
himel-shimul <himelshimul00@gmail.com>
2023-01-27 11:19:13 +0600

    fix issues

--- src/pages/Home/HeaderSearch/HeaderSearch.js
+++ src/pages/Home/HeaderSearch/HeaderSearch.js
@@ -14,6 +14,12 @@ const HeaderSearch = () => {
       key: "selection",
     },
   ]);
+  const handleGetLocation = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    console.log(name);
+  }
+  
 
   return (
     <div className="w-full mx-auto h-44 md:h-[550px] md:flex md:items-center justify-center bg-blue-900 relative" style={{ backgroundImage: `url(${SearchBannerImage})`,
@@ -25,13 +31,18 @@ const HeaderSearch = () => {
       </div>
       <div className="w-full md:w-3/5 px-2 mx-auto h-12 md:h-8 bg-white rounded-lg flex justify-between sm:mx-4 md:justify-around items-center lg:py-12 absolute top-14 md:top-80">
         <div>
+          <form onSubmit={handleGetLocation}>
           <input
+          
+            name="name"
             type="text"
             placeholder="Where are you going?"
-            className="headersearchinput border-none outline-none text-xs lg:text-lg"
+            className="py-2 border-none outline-none text-xs lg:text-lg"
           />
+          <input type="submit" value="Submit" className="px-2 md:px-4 rounded-sm bg-blue-700 text-white lg:text-lg py-2"/>
+          </form>
         </div>
-        <div>
+        {/* <div>
           {openDate && (
             <DateRange
               editableDateInputs={true}
@@ -41,8 +52,8 @@ const HeaderSearch = () => {
               className="absolute top-14"
             />
           )}
-        </div>
-        <span
+        </div> */}
+        {/* <span
           onClick={() => setOpenDate(!openDate)}
           className="headersearchtext  cursor-pointer text-xs lg:text-lg"
         >
@@ -50,18 +61,16 @@ const HeaderSearch = () => {
             date[0].endDate,
             "MM/dd/yyyy"
           )}`}
-        </span>
+        </span> */}
 
-        <div className="py-2 flex items-center">
+        {/* <div className="py-2 flex items-center">
           <span className="text-[10px] lg:text-lg">2 adults 2 children 1 room</span>
-        </div>
-        <div>
-          <Link to='/searchpage'>
+        </div> */}
+        {/* <div>
           <button className="px-2 md:px-4 rounded-sm bg-blue-700 text-white py-2">
             Search
           </button>
-          </Link>
-        </div>
+        </div> */}
       </div>
     </div>
   );
