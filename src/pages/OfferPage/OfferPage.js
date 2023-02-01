aebbb2f635ffba793b26c89863aacb5862908340
himel-shimul <himelshimul00@gmail.com>
2023-02-01 15:29:26 +0600

    seach context add

--- src/pages/OfferPage/OfferPage.js
+++ src/pages/OfferPage/OfferPage.js
@@ -5,10 +5,10 @@ import CityCard from './CityCard/CityCard';
 
 const OfferPage = () => {
     return (
-        <div className='w-[80%] mx-auto'>
+        <div className='w-[100%] mx-auto'>
             <HeaderSearch></HeaderSearch>
-            <h2 className='text-xl my-2 mt-20'>Explore more Early 2023 Deals</h2>
-            <div className="grid lg:grid-cols-3 gap-6">
+            <h2 className='text-xl my-2 mt-20 mx-4'>Explore more Early 2023 Deals</h2>
+            <div className="grid lg:grid-cols-3 gap-6 mx-4">
             <CityCard></CityCard>
             <CityCard></CityCard>
             <CityCard></CityCard>
