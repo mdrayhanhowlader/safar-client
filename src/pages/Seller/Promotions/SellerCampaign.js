24d708991af5f08025dfd02505c21ecc2d2118d0
mdrayhanhowlader <rhowlader5314@gmail.com>
2023-02-07 02:46:51 +0600

    added promotion page updated available campaign and registered page for offer

--- src/pages/Seller/Promotions/SellerCampaign.js
+++ src/pages/Seller/Promotions/SellerCampaign.js
@@ -4,23 +4,31 @@ import SellerAvailableCampaign from "./SellerAvailableCampaign";
 import SellerRegisteredCampaign from "./SellerRegisteredCampaign";
 
 const SellerCampaign = () => {
-  const [show, setShow] = useState("");
+  const [show, setShow] = useState("available-campaign");
   return (
     <div>
       <div className="m-8">
         <h2>Campaign Management</h2>
         <div className="border-1 border-slate-600 m-2">
           {/* offer nav */}
-          <div className="w-full p-3">
+          <div className="w-full py-3 text-slate-700">
             <ul className="flex justify-start gap-2">
               <li
-                className="bg-slate-400 p-2 cursor-pointer"
+                className={
+                  show === "available-campaign"
+                    ? " p-2 cursor-pointer border-b-2 border-orange-400"
+                    : "p-2 cursor-pointer"
+                }
                 onClick={() => setShow("available-campaign")}
               >
                 Available Campaign
               </li>
               <li
-                className="bg-slate-400 p-2 cursor-pointer"
+                className={
+                  show === "registered-campaign"
+                    ? " p-2 cursor-pointer border-b-2 border-orange-400 "
+                    : "p-2 cursor-pointer"
+                }
                 onClick={() => setShow("registered-campaign")}
               >
                 Registered Campaign
@@ -28,7 +36,7 @@ const SellerCampaign = () => {
             </ul>
           </div>
           {/* offer body */}
-          <div>
+          <div className="p-2 border-2 border-slate-300">
             {show === "available-campaign" && (
               <SellerAvailableCampaign></SellerAvailableCampaign>
             )}
