7192a6a2f109f43c763ed8fbcebc52e4b223c290
webnur <mdnuruli291@gmai.com>
2023-02-10 20:25:59 +0600

    manage seller page updated

--- src/pages/Seller/Products/ManageSellerProduct/ManageSellerProduct.js
+++ src/pages/Seller/Products/ManageSellerProduct/ManageSellerProduct.js
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
 import { HiPencilAlt } from "react-icons/hi";
@@ -5,9 +6,30 @@ import { HiPencilAlt } from "react-icons/hi";
 const ManageSellerProduct = () => {
   const [submenu, setSubmenu] = useState(false);
 
+  const handleSubMenu = () => {
+    setSubmenu(!submenu);
+  };
+
+  const {
+    data: organizerRooms,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["organizerRooms"],
+    queryFn: async () => {
+      const res = await fetch(
+        "https://safar-server-nasar06.vercel.app/rooms/get-all-rooms/9062086"
+      );
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  console.log(organizerRooms);
+
   return (
     <section className="py-4">
-      <h2 className="text-3xl">My Products</h2>
+      <h2 className="text-3xl">My Rooms</h2>
       {/* product table  */}
       <div className="rounded-lg border border-gray-200 mr-2 mt-5">
         <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
@@ -36,70 +58,66 @@ const ManageSellerProduct = () => {
               <th scope="col" className="px-6 py-4 font-medium text-gray-900">
                 Date
               </th>
-              <th
-                scope="col"
-                className="px-6 py-4 font-medium text-gray-900"
-              >Actions</th>
+              <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                Actions
+              </th>
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-100 border-t border-gray-100">
-            <tr className="hover:bg-gray-50">
-              <td className="px-6 py-4">
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
+            {isLoading ? (
+              <td>
+                <span>Loading...</span>
               </td>
-              <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
-                <div className="relative h-12 w-12">
-                  <img
-                    className="h-full w-full rounded-md object-cover object-center"
-                    src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                  <span className="absolute right-0 bottom-0 h-2 w-2 rounded-full bg-green-400 ring ring-white"></span>
-                </div>
-                <div className="text-sm">
-                  <div className="text-gray-400">Product live link</div>
-                </div>
-              </th>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  product title will here
-                </span>
-              </td>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  228390277
-                </span>
-              </td>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  $500
-                </span>
-              </td>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  10
-                </span>
-              </td>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  10-02-2023
-                </span>
-              </td>
+            ) : (
+              organizerRooms.map((room) => (
+                <tr key={room._id} className="hover:bg-gray-50">
+                  <td className="px-6 py-4">
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </td>
+                  <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
+                    <div className="relative h-12 w-12">
+                      <img
+                        className="h-full w-full rounded-md object-cover object-center"
+                        src={room.images[0].url}
+                        alt=""
+                      />
+                      <span className="absolute right-0 bottom-0 h-2 w-2 rounded-full bg-green-400 ring ring-white"></span>
+                    </div>
+                    <div className="text-sm">
+                      <div className="text-gray-400">Product live link</div>
+                    </div>
+                  </th>
+                  <td className="px-6 py-4">
+                    <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
+                      {room.name}
+                    </span>
+                  </td>
+                  <td className="px-6 py-4">
+                    <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
+                      {room.room_no}
+                    </span>
+                  </td>
+                  <td className="px-6 py-4">
+                    <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
+                      {room.price}
+                    </span>
+                  </td>
+                  <td className="px-6 py-4">
+                    <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
+                      {room.sleep}
+                    </span>
+                  </td>
+                  <td className="px-6 py-4">
+                    <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
+                      10-02-2023
+                    </span>
+                  </td>
 
-              <td className="px-6 py-4">
-                {/* <div className="flex justify-end gap-4">
-                  <a href="/">
-                    <FaTrashAlt className="h-6 w-6"></FaTrashAlt>
-                  </a>
-                  <a href="/">
-                    <HiPencilAlt className="h-6 w-6"></HiPencilAlt>
-                  </a>
-                </div>  */}
-
-                 <div className="absolute">
-                  <p onClick={() => setSubmenu(!submenu)} className="font-xxl font-bold cursor-pointer ">More</p>
+                  <td className="px-6 py-4">
+                    {/* <div className="absolute">
+                  <p onClick={() => handleSubMenu(true)} className="font-xxl font-bold cursor-pointer ">More</p>
                   <div className={`${submenu ? 'visible' : 'hidden'} z-10 relative bg-white shadow-md `}>
                   <ul>
                   <li className="py-2 px-4 hover:bg-blue-500 hover:text-white rounded-md"><a href="/">Edit</a></li>
@@ -109,123 +127,20 @@ const ManageSellerProduct = () => {
                   </ul>
                   </div>
  
-                </div>
+                </div>   */}
 
-                
-              </td>
-            </tr>
-            <tr className="hover:bg-gray-50">
-              <td className="px-6 py-4">
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </td>
-              <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
-                <div className="relative h-12 w-12">
-                  <img
-                    className="h-full w-full rounded-md object-cover object-center"
-                    src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                  <span className="absolute right-0 bottom-0 h-2 w-2 rounded-full bg-green-400 ring ring-white"></span>
-                </div>
-                <div className="text-sm">
-                  <div className="text-gray-400">Product live link</div>
-                </div>
-              </th>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  product title will here
-                </span>
-              </td>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  228390277
-                </span>
-              </td>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  $500
-                </span>
-              </td>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  10
-                </span>
-              </td>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  10-02-2023
-                </span>
-              </td>
-
-              <td className="px-6 py-4">
-                <div className="flex justify-end gap-4">
-                  <a href="/">
-                    <FaTrashAlt className="h-6 w-6"></FaTrashAlt>
-                  </a>
-                  <a href="/">
-                    <HiPencilAlt className="h-6 w-6"></HiPencilAlt>
-                  </a>
-                </div>
-              </td>
-            </tr>
-            <tr className="hover:bg-gray-50">
-              <td className="px-6 py-4">
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </td>
-              <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
-                <div className="relative h-12 w-12">
-                  <img
-                    className="h-full w-full rounded-md object-cover object-center"
-                    src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                  <span className="absolute right-0 bottom-0 h-2 w-2 rounded-full bg-green-400 ring ring-white"></span>
-                </div>
-                <div className="text-sm">
-                  <div className="text-gray-400">Product live link</div>
-                </div>
-              </th>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  product title will here
-                </span>
-              </td>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  228390277
-                </span>
-              </td>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  $500
-                </span>
-              </td>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  10
-                </span>
-              </td>
-              <td className="px-6 py-4">
-                <span className="inline-flex items-center gap-1   px-2 py-1 text-sm font-semibold">
-                  10-02-2023
-                </span>
-              </td>
-
-              <td className="px-6 py-4">
-                <div className="flex justify-end gap-4">
-                  <a href="/">
-                    <FaTrashAlt className="h-6 w-6"></FaTrashAlt>
-                  </a>
-                  <a href="/">
-                    <HiPencilAlt className="h-6 w-6"></HiPencilAlt>
-                  </a>
-                </div>
-              </td>
-            </tr>
+                    <div className="flex justify-end gap-4">
+                      <a href="/">
+                        <FaTrashAlt className="h-4 w-4"></FaTrashAlt>
+                      </a>
+                      <a href="/">
+                        <HiPencilAlt className="h-4 w-4"></HiPencilAlt>
+                      </a>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            )}
           </tbody>
         </table>
       </div>
