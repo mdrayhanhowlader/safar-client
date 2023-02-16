70e9425a2e0372b37341602b2c6ad4b2812b4247
webnur <mdnuruli291@gmai.com>
2023-02-16 22:18:30 +0600

    manageHotels and organizer page edit

--- src/pages/Admin/Properties/Hotels/ManageHotels/ManageHotels.js
+++ src/pages/Admin/Properties/Hotels/ManageHotels/ManageHotels.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React, { useState } from "react";
 
 const ManageHotels = () => {
-    return (
-        <div>
-            <h2 className="text-2xl">Manage hotel page</h2>
-        </div>
-    );
+  const [submenu, setSubmenu] = useState(false);
+  const {
+    data: hotels,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["organizers"],
+    queryFn: async () => {
+      const res = await fetch(
+        "https://safar-server-nasar06.vercel.app/destination/get-all-destinations"
+      );
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  console.log(hotels);
+
+  return (
+    <section className="m-4">
+      <h2 className="text-2xl font-bold">All Hotels </h2>
+      {/* table all users  */}
+      
+    </section>
+  );
 };
 
 export default ManageHotels;
